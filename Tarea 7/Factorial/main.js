26bff5f318d5964cf393945447ce5b6022537e60
Atenea Gutierrez <89951290+MissionApolo18@users.noreply.github.com>
2023-09-25 00:47:51 -0600

    Corrección del algoritmo
    Pasó de ser por iteración a ser un algoritmo recursivo

--- Tarea 7/Factorial/main.js
+++ Tarea 7/Factorial/main.js
@@ -1,17 +1,11 @@
-//Algoritmo de Fibonacci
+//Algoritmo Factorial
 //Hecho por Atenea Gutiérrez / MissionApolo18
-let n = prompt('Ingresé el número que se quiere obtener el factorial: ');
-const i = 0, a = n - 1, m = 0;
+var num = prompt('Ingresé el número que se quiere obtener el factorial: ');
 
-function Factorial(n) {
-  while (a != 0) {
-    a = n - 1;
-    m = n * a;
-    i = i + m;
-    n = n - 1;
-    if (n = 1 || n < 3) {
-      n = 1;
-    }
+function Factorial(num) {
+  if num = 1 {
+    return 1
+  } else {
+    return n * Factorial(num - 1)
   }
-  return i
 }
