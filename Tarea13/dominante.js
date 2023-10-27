b2b4c1ed53d4a1121044cd25983a5a6e5e8787ee
Atenea Gutierrez <89951290+MissionApolo18@users.noreply.github.com>
2023-10-26 22:57:22 -0600

    Tarea 13

---
+++ Tarea13/dominante.js
@@ -0,0 +1,13 @@
+let vari = [1,21,4,7,5];
+function domi(vari) {
+    const Arr = [];
+    for (let i = 0; i < array.length; i++) {
+        if (i<(i+1)) {
+            return Arr[i];
+        } else {
+            return Arr[vari[i+1]];
+        }
+    }
+}
+
+console.log(domi);
